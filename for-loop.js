0bc6ac5ddb0f52e207f433a158687b6d3dd7a847
ylhoony <yl.hoony@gmail.com>
2016-11-23 06:32:24 +0000

    Initial commit for Javascripting folder

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+var total = 0;
+var limit = 10;
+
+for (var i = 1; i < limit; i++) {
+  total += i;
+}
+console.log(total);
---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (width, length, height) {
+  return (length * height) + width;
+}
+console.log( math(53, 61, 67) );
